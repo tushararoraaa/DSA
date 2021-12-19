3470561c4321d71018bac609f035e2a76939e2a5
tushararoraaa <64618178+tushararoraaa@users.noreply.github.com>
2021-12-19 17:16:18 +0530

    initial commit

--- New75/DeleteNode.java
+++ New75/DeleteNode.java
@@ -1,5 +1,10 @@
 public class DeleteNode {
 
+    // HERE WE ARE GIVEN ACCESS ONLY TO NODE TO BE DELETED
+    // NO ACCESS TO HEAD
+    // TO BE DELETED NODE IS NOT TAIL NODE
+    // NODE STRUCTURE :- CONTAINS NEXT AND INT VAL;
+
     public static void deleteNode(ListNode node) {
         int temp = node.next;
         node.val = temp;
