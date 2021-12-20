b5a551878ccc57b89a7d5b2adb11fcf21f8d97ac
tushararoraaa <64618178+tushararoraaa@users.noreply.github.com>
2021-12-20 23:11:20 +0530

    initial commit

--- day021/LastIndex.java
+++ day021/LastIndex.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LastIndex {
+public class LastIndex { // FIRST FIND THEN CALL
 
     public static int lastIndex(int[] arr, int idx, int x) {
         if (idx < 0) {
@@ -14,6 +14,23 @@ public class LastIndex {
 
     }
 
+    public static int lastIndex_better(int[] arr, int idx, int x) { // FIRST CALL THEN FIND
+        if (idx == arr.length) {
+            return -1;
+        }
+
+        int li = lastIndex_better(arr, idx + 1, x);
+        if (li == -1) {
+            if (arr[idx] == x) {
+                return idx;
+            } else {
+                return li;
+            }
+        } else {
+            return li;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
@@ -24,5 +41,7 @@ public class LastIndex {
         int x = scn.nextInt();
         int li = lastIndex(arr, arr.length - 1, x);
         System.out.println(li);
+        int li2 = lastIndex_better(arr, 0, x);
+        System.out.println(li2);
     }
 }
